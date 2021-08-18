9337c493dece5d9a3cdd35fb34474ba5f4c6fa22
Fabiano Martins <fabismaple01@gmail.com>
2021-08-18 17:44:24 -0300

    Adjust Expenses Components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const handleClick = () => {
+    setTitle("Updated");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">R${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   );
 };
