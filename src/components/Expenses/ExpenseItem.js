f55a5794c9de3e74fbdcefa1c3a18649e014b8ac
Fabiano Martins <fabismaple01@gmail.com>
2021-08-24 15:41:41 -0300

    Add open and close button form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const handleClick = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
-
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">R${props.amount}</div>
-      </div>
-      <button onClick={handleClick}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">R${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -24,8 +24,8 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate),
+      amount: +enteredAmount,
+      date: new Date(enteredDate + "T00:00"),
     };
 
     props.onSaveExpenseData(expenseData);
@@ -67,6 +67,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
